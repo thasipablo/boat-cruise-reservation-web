2791fec3087863616451d3c9e2b80a0f95534018
unknown <macherki.nessrine@yahoo.com>
2024-02-27 23:26:15 +0100

    add registration form with devise

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'; // Add this line if it's missing
 import './App.css';
+import Registration from './components/auth/Registration';
 
 function App() {
   return (
     <div>
-    
+      <Registration />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
