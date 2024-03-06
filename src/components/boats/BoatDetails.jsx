95b720d09f0fc59e412a5182d9c329be6f94952e
over-geek <kenagy100@gmail.com>
2024-03-06 05:32:56 +0000

    Add boat details page

---
+++ src/components/boats/BoatDetails.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { MdOutlinePlayArrow } from 'react-icons/md';
+
+const BoatDetails = ({ boat, onBack }) => {
+  const handleBack = () => {
+    onBack();
+  };
+  return (
+    <div className="boat_details">
+      <div className="boat_details_img_container">
+        <div className="selected_boat_img"><img src={boat.image} alt={boat.name} /></div>
+        <div className="splide__arrows">
+          <button type="button" className="splide__arrow splide__arrow--prev" onClick={handleBack}>
+            <span className="react-icon react-icon--next">
+              <MdOutlinePlayArrow />
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="boat_info">
+        <div><h1>{boat.name}</h1></div>
+        <ul className="boat_expense">
+          <li>
+            <p>finance_fee</p>
+            {boat.finance_fee}
+          </li>
+          <li>
+            <p>Option to purchase fee</p>
+            {boat.option_to_purchase_fee}
+          </li>
+          <li>
+            <p>Duration</p>
+            {boat.duration}
+          </li>
+          <li>
+            <p>Total amount payable</p>
+            {boat.amount_payable}
+          </li>
+          <li>
+            <p>Availability</p>
+            {boat.availability ? 'Available' : 'Not Available'}
+          </li>
+        </ul>
+        <div><p>{boat.description}</p></div>
+        <div className="reservation_btn_container">
+          <Link to="/reservation" className="btn btn-primary">Reserve</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BoatDetails.propTypes = {
+  boat: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    availability: PropTypes.bool.isRequired,
+    finance_fee: PropTypes.number.isRequired,
+    option_to_purchase_fee: PropTypes.number.isRequired,
+    duration: PropTypes.number.isRequired,
+    amount_payable: PropTypes.number.isRequired,
+  }).isRequired,
+  onBack: PropTypes.func.isRequired,
+};
+
+export default BoatDetails;
